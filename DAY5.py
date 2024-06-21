7ba6dbb8003f65373c8e23a94b3ea6c7b4edd1a8
Vb2508 <vivekbirajdar77@gmail.com>
2024-06-21 19:00:50 +0530

    dictionary

---
+++ DAY5.py
@@ -0,0 +1,30 @@
+# list = [1,2,3,5]
+# a,b,c,d = list
+# print (a)
+
+
+
+
+# names = [['a','b'] , ['c','d']]
+# x,y = names
+# print(x)
+# print (x[0])
+# print (y[0])
+
+
+# names = {'a','b'}
+# for item in names:
+#  print(item)
+
+
+# dupNames=["a","b","c","a","b"] 
+# vivek= set(dupNames)
+# print(dupNames, vivek)
+
+
+dictt= {'username':'cricket100', 'password':'peace143'}
+vivek= dictt['username']
+birajdar = dictt['password']
+print (birajdar, vivek)
+
+
