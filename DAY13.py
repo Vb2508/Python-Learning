42f3da29abea3687b0667bd9d6001d5ff8a73392
Vb2508 <vivekbirajdar77@gmail.com>
2024-07-03 19:17:04 +0530

    replace contact using value

--- DAY13.py
+++ DAY13.py
@@ -19,11 +19,16 @@ class Contact:
              self.ContatctList.pop(index)
              print(item,"removed contact")
 
-    
+    def replaceContact(self,id,name):
+        for item in self.ContatctList:
+            if id==item["id"]:
+                print(item)
+                item.update({"ContactName":name})
+                print(item,"replaced number")
 B=Contact()
 B.addcontact({"id":1,"ContactName":"vivek b","ContactNumber":"9049391822"})
 B.addcontact({"id":2,"ContactName":"vivek b1","ContactNumber":"8237253494"})
 B.addcontact({"id":3,"ContactName":"Malak","ContactNumber":"9518937690"})
-
+B.replaceContact(2,"gggggggg")
 B.removeContact(1)
 B.showcontactlist()
